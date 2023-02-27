a6aa28d662e2eb64dd29475ba3f3424015657e93
Steve <stevekoech8@gmail.com>
2023-02-27 11:55:28 +0300

    Completed Assignment

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  #returns the first and last name of an actor.
+  def full_name
+    full_name = "#{self.first_name} #{self.last_name}"
+    full_name
+  end
+
+  #lists all of the characters that actor has alongside the show that the character is in.
+  def list_roles
+    roles = []
+    self.characters.each do |character|
+        roles<< "#{character.name} - #{character.show.name}"
+    end
+    roles
+  end
 end
